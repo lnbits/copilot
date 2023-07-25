e8113bf92397de029c1c4334106872dfd84af7d9
dni ⚡ <office@dnilabs.com>
2023-07-25 09:02:41 +0200

    fix model validation for get_copilot

--- models.py
+++ models.py
@@ -37,27 +37,27 @@ class CreateCopilotData(BaseModel):
 
 class Copilot(BaseModel):
     id: str
-    user: str = Query(None)
-    title: str = Query(None)
-    lnurl_toggle: int = Query(0)
-    wallet: str = Query(None)
-    animation1: str = Query(None)
-    animation2: str = Query(None)
-    animation3: str = Query(None)
-    animation1threshold: int = Query(None)
-    animation2threshold: int = Query(None)
-    animation3threshold: int = Query(None)
-    animation1webhook: str = Query(None)
-    animation2webhook: str = Query(None)
-    animation3webhook: str = Query(None)
-    lnurl_title: str = Query(None)
-    show_message: int = Query(0)
-    show_ack: int = Query(0)
-    show_price: str = Query(None)
-    amount_made: int = Query(0)
-    timestamp: int = Query(0)
-    fullscreen_cam: int = Query(0)
-    iframe_url: str = Query(None)
+    user: str
+    title: str
+    lnurl_toggle: int
+    wallet: str
+    animation1: str
+    animation2: str
+    animation3: str
+    animation1threshold: str
+    animation2threshold: str
+    animation3threshold: str
+    animation1webhook: str
+    animation2webhook: str
+    animation3webhook: str
+    lnurl_title: str
+    show_message: int
+    show_ack: int
+    show_price: str
+    amount_made: int
+    timestamp: int
+    fullscreen_cam: int
+    iframe_url: str
 
     def lnurl(self, req: Request) -> str:
         url = req.url_for("copilot.lnurl_response", cp_id=self.id)
