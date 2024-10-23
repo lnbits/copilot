f5b4f428995ccaac1879b9f35c8a9eb12bbfb724
dni ⚡ <office@dnilabs.com>
2024-10-23 16:09:21 +0200

    feat: update to lnbits 1.0.0 (#21)

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.helpers import template_renderer
@@ -12,13 +11,10 @@ def copilot_renderer():
     return template_renderer(["copilot/templates"])
 
 
-templates = Jinja2Templates(directory="templates")
-
-
 @copilot_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
     return copilot_renderer().TemplateResponse(
-        "copilot/index.html", {"request": request, "user": user.dict()}
+        "copilot/index.html", {"request": request, "user": user.json()}
     )
 
 
